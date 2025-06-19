c58372bc7786ae3479a2edf7c04111ad3f470d40
Asante.dev <davidameyaw607@gmail.com>
2025-06-19 22:23:13 +0000

    Add client directive: Include "use client" directive in the Error component to enable client-side rendering.

--- app/error.js
+++ app/error.js
@@ -1,4 +1,6 @@
-export default function Error({error, reset}) {
+"use client";
+
+export default function Error({ error, reset }) {
   return (
     <main className='flex justify-center items-center flex-col gap-6'>
       <h1 className='text-3xl font-semibold'>Something went wrong!</h1>
